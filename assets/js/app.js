f101711df0a8031b14122e58ee5c3d6a9d7258d6
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-03-29 11:09:22 +0200

    Latitude and langtitude

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+//Test to see if JS is connected to HTML: succes
+// console.log("hello, world");
+
+//Code from w3school: https://www.w3schools.com/html/html5_geolocation.asp
+
+/* const paragraph = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else {
+    paragraph.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  paragraph.innerHTML =
+    "Latitude: " +
+    position.coords.latitude +
+    "<br>Longitude: " +
+    position.coords.longitude;
+}
+
+function showError(error) {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      paragraph.innerHTML =
+        "Why the fuck would you not let me see your position? What are you hidding?";
+      break;
+    case error.POSITION_UNAVAILABLE:
+      paragraph.innerHTML =
+        "Where the fuck are you, that your location is unavailable";
+      break;
+    case error.TIMEOUT:
+      paragraph.innerHTML = "I can no longer see your location.";
+      break;
+    case error.UNKNOWN_ERROR:
+      paragraph.innerHTML = "Something went wrong, don't know what";
+      break;
+  }
+}
+ */
